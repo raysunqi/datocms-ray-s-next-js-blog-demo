40b573ad5d4b6323ce01d9d47d39a6b63ccd52b1
Ray Sun <raysunqi@gmail.com>
2021-05-26 18:19:38 -0700

    test

--- components/avatar.js
+++ components/avatar.js
@@ -1,11 +1,17 @@
 export default function Avatar({ name, picture }) {
   return (
     <div className="flex items-center">
-      <img
-        src={picture.url}
-        className="w-12 h-12 rounded-full mr-4"
-        alt={name}
-      />
+    <div>
+     {picture ? `<img
+            src={picture.url}
+            className="w-12 h-12 rounded-full mr-4"
+            alt={name}
+          />`: ''}.
+    </div>
+    {/*  */}
+        
+
+        
       <div className="text-xl font-bold">{name}</div>
     </div>
   )
--- next.config.js
+++ next.config.js
@@ -8,4 +8,7 @@ module.exports = {
     NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN:
       process.env.NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN,
   },
+  images: {
+    domains: ['restcountries.eu'],
+  }
 }
